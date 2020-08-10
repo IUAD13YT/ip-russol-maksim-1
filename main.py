1ab513ef8964d4e1a136a6061c3127204015e72e
IUAD13YT <iuad13yt@gmail.com>
2020-08-10 21:07:27 +0400

    fist commit

---
+++ main.py
@@ -0,0 +1,41 @@
+print("Hello World")
+
+print(2**2)
+
+Hello_World='2*3'
+
+a = 9
+a += 1
+print(a**3)
+#переменная типа - float
+f=1/3
+print(type(f))
+
+#название типов данных'''
+first="age "
+second=" 29"
+second=str(second)
+print(first+second)
+
+#primer 1
+a=1.3
+b=36
+c=a+b
+print(c)
+#primer 2
+s=float(3.8)+6
+print(s)
+
+'''ввод/вывод данных'''
+#name = input("Enter your name: ")
+#print("Hello world " + name)
+
+#ввод/вывод данных 2'''
+'''name = input("Enter integer: ")
+name = int(name)
+print(name)
+print(type(name))#'''
+
+age = 18
+name = "Vasya"
+print("My name is", name, ".I'm", age, "years old.")
