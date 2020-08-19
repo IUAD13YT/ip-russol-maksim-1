d06ebb5930bcaaceeaed351455cacc35c4d99fc6
IUAD13YT <iuad13yt@gmail.com>
2020-08-19 19:00:39 +0400

    zadolbalo

--- Class/Lesson3.py
+++ Class/Lesson3.py
@@ -107,3 +107,169 @@ f={} #А так получится словарь
 print('type({})-->', type(f)) #<class 'dict'>
 # операции с множествами
 ghghng
+
+
+# # string = 'Hello'
+# # print(string[1:5:1])
+#
+# string = 'Hello'
+# string_test = 'aaajjjjfffffdkkdkdsl'
+# a = 'Start'
+# b = 'Stop'
+# c = 'Step'
+# # String[a:b:c] индексация начинается с 0.
+#
+# # Методы строк
+# print(string.find('ll'), len(string_test))
+# # .title() .upper() .lower() .find() .etc
+#
+# #Форматирование строк
+# name = 'Eric'
+# surname = 'Smith'
+# lastname = 'Jacson'
+# print('Welcome ' + name + ' ' + surname + '!')
+#
+# # Новый метод форматирования
+# print('Welcome {} {}!' .format(name, surname))
+# print('Welcome {2} {0}!' .format(name, surname, lastname))
+
+# name = 'Eric'
+# age = '74'
+# print(f'Hello, {name}. You are {age}.')
+
+# emp_list = []   #Пустой список
+my_list = [1, 3, 5, 3.45, 'ddd', 's', '333']
+# print('my_list=', my_list)
+# print(max(str(my_list)))
+# print((my_list[0]))
+# print((my_list[-1]))
+# # Срезы
+print(my_list[0:-3])
+# #Конкатенация
+# print(my_list[0:3] + [7,8,9])
+# #Мультипликация
+# print([3,'4'] * 3)
+#
+# my_list = [1, 3, 5, 3.45, 'ddd', 's', '333']
+# print('ssss' in my_list)
+#
+# my_list = [1, [1,[3]]]
+# print(my_list[1][1][0])
+#
+# my_list = [1, 3, 5, 3.45, 'ddd', 's', '333']
+#
+# my_list.append(['1',3.2])
+# print(my_list)
+#
+# lst = [1,2,3]
+# lst.append(4) # добавить в конец списка
+# lst.pop() # удалить последний объект
+# lst.pop(1) # удилить элемент с индексом 1
+# print(lst)
+
+
+# Кортежи
+#
+# t = ()
+#
+# t = (2)
+# print(t)
+#
+# t = (2,)
+# print(t)
+# t = 2,
+# print(t)
+#
+
+# x = [1,2,3]
+# print(x)
+# print(x.pop())
+# print(x)
+# del x[0]
+# print(x)
+
+# Перебор элементов по индексам
+# fruits = ['apple','banana', 'mango']
+# i = 0
+# while len (fruits) > i:
+#     print('fruits = ', fruits[i])
+#     i = i + 1
+
+
+#
+# fruits = ['apple', 'banana', 'mango']
+# for vegetables in fruits:
+#     print('fruits = ', vegetables)
+
+# for el in 'Hello':
+#     print('el =', el)
+#     print()
+#     for t_el in 1,2,3,4,5,10:
+#         print('------')
+#         print('t_el = ', t_el)
+#         print()
+
+#
+# x = {'x', 'y', 'z'}
+# #  {Ключ : Значение}
+# print(x)
+# x['y'] = '1'
+# print(x)
+# x['y'] = {'1': 1}
+# print(x)
+#
+# f = {'y':[], 'z':2}
+# print(f)
+# f.pop('y')
+
+#Цикл по словарю
+# f = {}
+# print(f)
+# f['y'] = '1'
+# for key, value in f.items():
+#  print(key, value)
+
+# for key in f.keys():
+#      print(key)
+# #
+# for value in f.values():
+#     print(value)
+# # удаляет элемент с и возвращает его значение
+#     print(f.pop('c'))
+#     print(f)
+# # удаляет и возвращает пару (ключ, значение)
+#     print(f.popitem())
+# # удаляет произвольную пару (ключ, значение)
+#
+# x = {'x': '1'}
+# print(x.get('x'))
+# print(x['x'])
+#Множества
+
+# a = set()
+# print('a =', a)
+# b = set(['a','b','c','c','a','e'])
+# print('b =', b)
+# c = set('hello')
+# print('d =', c)
+# d = {'a','b','c','d'}
+# print('d=',d)
+# f = {}
+# print('type({})-->',type(f))
+# print(len(b))
+# print("b' in b -->", 'b' in b)
+
+#issubset  нахождение подмножества
+#issuperset нахождение надмножества
+#union объедение множеств
+#intersection  точка пересечения
+#difference отличие множеств
+#symmetric_difference  Возвращает симметричную разницу
+                     # — только те элементы, которые есть либо в одном,
+                        # либо в другом, но не в обоих множествах.
+
+
+a = set (['a', '3', '5'])
+b = set(['a','3',6])
+print(a)
+print(b.issubset(a))
